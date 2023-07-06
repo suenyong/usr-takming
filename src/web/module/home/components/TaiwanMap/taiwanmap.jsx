ed88648ff220364d3960b9252a1253086101c8ff
Ting1113 <D11016114@gs.takming.edu.tw>
2023-07-07 00:30:27 +0800

    tawain2

--- src/web/module/home/components/TaiwanMap/taiwanmap.jsx
+++ src/web/module/home/components/TaiwanMap/taiwanmap.jsx
@@ -1,46 +1,41 @@
-import "./styles/taiwanese.css";
+// TaiwanMap.jsx
+
 import React, { useState } from "react";
 import taiwan from "./images/taiwan.png";
+import taipei from "./images/taipei.png";
+import "./styles/taiwanese.css";
 
 const TaiwanMap = () => {
-  const [selectedButton, setSelectedButton] = useState(null);
+  const [showTaipei, setShowTaipei] = useState(false);
 
-  const handleButtonClick = (buttonId) => {
-    setSelectedButton(buttonId);
+  const handleButtonClick = () => {
+    setShowTaipei(true);
   };
 
   return (
-    <div style={{ textAlign: "right" }}>
+    <div className="taiwan-map">
       <div className="hr-line"></div>
-      <h1 style={{ textAlign: 'center', color: 'navy' }}>服務足跡</h1>
-      {/* 台灣地圖圖片 */}
-      <div style={{ display: "inline-block", textAlign: "left" }}>
-        <img
-          src={taiwan}
-          alt="台灣地圖"
-          style={{ width: "50%", position: "relative", right: "0" }}
-        />
+      <h1 className="map-title">服務足跡</h1>
+
+      <div className="map-container">
+        {/* 台灣地圖圖片 */}
+        {!showTaipei && <img src={taiwan} alt="台灣地圖" className="map-image" />}
+
+        {/* 縣市按鈕 */}
+        {!showTaipei && (
+          <button onClick={handleButtonClick} className="city-button">
+            台北
+          </button>
+        )}
+        {/* 其他縣市按鈕 */}
       </div>
 
-      {/* 縣市按鈕 */}
-      <button
-        onClick={() => handleButtonClick(1)}
-        style={{ position: "absolute", top: "100px", left: "200px" }}
-      >
-        縣市按鈕1
-      </button>
-      <button
-        onClick={() => handleButtonClick(2)}
-        style={{ position: "absolute", top: "200px", left: "300px" }}
-      >
-        縣市按鈕2
-      </button>
-      {/* 其他縣市按鈕 */}
-
       {/* 根據選中的按鈕顯示照片 */}
-      {selectedButton === 1 && <img src="photo1.jpg" alt="縣市按鈕1的照片" />}
-      {selectedButton === 2 && <img src="photo2.jpg" alt="縣市按鈕2的照片" />}
-      {/* 其他照片 */}
+      {showTaipei && (
+        <div className="map-container">
+          <img src={taipei} alt="台北照片" className="map-image" />
+        </div>
+      )}
     </div>
   );
 };
