9ad35ac5f628ad3371a46f2584ddbe941921c5e5
Ting1113 <D11016114@gs.takming.edu.tw>
2023-07-07 20:27:06 +0800

    taiwanmapup

--- src/web/module/home/components/TaiwanMap/taiwanmap.jsx
+++ src/web/module/home/components/TaiwanMap/taiwanmap.jsx
@@ -1,68 +1,63 @@
-// TaiwanMap.jsx
-
 import React, { useState } from "react";
-import taiwan from "./images/taiwan.png";
-import taipei from "./images/taipei.png";
 import "./styles/taiwanese.css";
-
-import testBackground from "./images/南投.png"
-import testBackground_1 from "./images/台中.png"
+import taiwan from "./images/台灣拚完.png";
+import Taichung from "./images/Taichung.png"; // 台中
+import Keelung from "./images/Keelung.png"; // 基隆
+import Philippines from "./images/Philippines.png";//菲律賓
+import Taitung from "./images/Taitung.png";//台東
 
 const TaiwanMap = () => {
-  const [showTaipei, setShowTaipei] = useState(false);
-  const [imgPath, setImgPath] = useState(testBackground)
+  const [imgPath, setImgPath] = useState(taiwan);
 
-  const handleButtonClick = () => {
-    setShowTaipei(true);
+  const handleMouseOver = (newImgPath) => {
+    setImgPath(newImgPath);
   };
 
-
-  const printA = () => {
-    setImgPath(testBackground_1)
-  }
-
-  const printB = () => {
-    setImgPath(testBackground)
-  }
+  const handleMouseOut = () => {
+    setImgPath(taiwan);
+  };
 
   return (
-    <div className="taiwan-map">
-
-    <button 
-      onMouseOver={printA} 
-      onMouseOut={printB}
-      // onMouseOver={() => {setImgPath(testBackground_1)}} 
-      // onMouseOut={() => {setImgPath(testBackground)}}
-      style={{width: '100px', height: '100px'}}
-      >
-        123123 
-    </button>
-
-    <img src={imgPath} alt="123" />
-
+    <>
       <div className="hr-line"></div>
       <h1 className="map-title">服務足跡</h1>
+      <div className="taiwan-map">
+        <img src={imgPath} alt="123" style={{ width: "25%" }} />
+        <button
+          onMouseOver={() => handleMouseOver(Taichung)}
+          onMouseOut={handleMouseOut}
+          className="Taichung-button"
+        >
+          台中
+        </button>
 
-      <div className="map-container">
-        {/* 台灣地圖圖片 */}
-        {!showTaipei && <img src={taiwan} alt="台灣地圖" className="map-image" />}
+        <button
+          onMouseOver={() => handleMouseOver(Keelung)}
+          onMouseOut={handleMouseOut}
+          className="Keelung-button"
+        >
+          基隆
+        </button>
 
-        {/* 縣市按鈕 */}
-        {!showTaipei && (
-          <button onClick={handleButtonClick} className="city-button">
-            台北
-          </button>
-        )}
-        {/* 其他縣市按鈕 */}
+        <button
+          onMouseOver={() => handleMouseOver(Philippines)}
+          onMouseOut={handleMouseOut}
+          className="Philippines-button"
+        >
+          菲律賓
+        </button>
+
+        <button
+          onMouseOver={() => handleMouseOver(Taitung)}
+          onMouseOut={handleMouseOut}
+          className="Taitung-button"
+        >
+          台東
+        </button>
+
+        <img src={Philippines} alt="Philippines" className="Philippines-image" />
       </div>
-
-      {/* 根據選中的按鈕顯示照片 */}
-      {showTaipei && (
-        <div className="map-container">
-          <img src={taipei} alt="台北照片" className="map-image" />
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
