1b925136393a7269631e9b803945767e064e2910
friendship0105 <tom9884661@gmail.com>
2023-07-09 01:20:45 +0800

    Service1

--- src/web/module/about_us/screens/Service.jsx
+++ src/web/module/about_us/screens/Service.jsx
@@ -4,28 +4,20 @@ import tw from "../images/台灣拚完.png";
 
 export default function Service() {
   return (
-    <div>
-      <h1
-        style={{
-          textAlign: "center",
-          fontSize: "40px",
-          fontFamily: "Arial, sans-serif",
-        }}
-      >
-        服務據點
-      </h1>
-      <div className="service-container">
-        <img src={tw} alt="台灣拚完圖片" />
-        <div>
-          <h1>國內</h1>
-          <div class="dot-buttons">
-            <button className="dot-button"><span>台北</span></button>
-            <button className="dot-button"><span>新北</span></button>
-            <button className="dot-button"><span>基隆</span></button>
-            <button className="dot-button"><span>桃園</span></button>
-            <button className="dot-button"><span>全部</span></button>
+    <div className="service-container">
+      <h1 className="service-title">服務據點</h1>
+      <div className="service-content">
+      <div className="service-text">
+          <h1 className="service-heading">國內</h1>
+          <div className="button-container">
+            <button className="service-button">台北</button>
+            <button className="service-button">新北</button>
+            <button className="service-button">基隆</button>
+            <button className="service-button">桃園</button>
+            <button className="service-button">全部</button>
           </div>
         </div>
+        <img src={tw} alt="台灣拚完圖片" className="service-image" />
       </div>
     </div>
   );
