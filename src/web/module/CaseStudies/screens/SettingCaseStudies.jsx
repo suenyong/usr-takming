7cffa6991077342d41b5f42e1b373a0ffe93c352
suenyng <su.yu.grace@gmail.com>
2023-07-08 21:40:18 +0800

    CaeseStudise content all

--- src/web/module/CaseStudies/screens/SettingCaseStudies.jsx
+++ src/web/module/CaseStudies/screens/SettingCaseStudies.jsx
@@ -1,17 +1,27 @@
 export const SettingCaseStudies = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    vertical: true,
-    verticalSwiping: true,
-    swipeToSlide: true,
-    beforeChange: function(currentSlide, nextSlide) {
-      console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: function(currentSlide) {
-      console.log("after change", currentSlide);
-    },
-    arrows: false,
+    // dots: true,
+    // infinite: true,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    // vertical: true,
+    // verticalSwiping: true,
+    // swipeToSlide: true,
+    // beforeChange: function(currentSlide, nextSlide) {
+    //   console.log("before change", currentSlide, nextSlide);
+    // },
+    // afterChange: function(currentSlide) {
+    //   console.log("after change", currentSlide);
+    // },
+    // arrows: false,
+
+  autoPlay:true, // 自动播放
+  interval:3000 ,// 每个项之间的切换间隔时间（以毫秒为单位）
+  showArrows:true, // 显示箭头导航
+  showIndicators:true, // 显示指示器导航
+  showThumbs:true, // 显示缩略图导航
+  infiniteLoop:true, // 循环播放
+  stopOnHover:true, // 鼠标悬停时停止播放
+  swipeable:true, // 允许通过滑动手势进行切换
+  dynamicHeight:false // 根据内容自适应高度
 
 };
