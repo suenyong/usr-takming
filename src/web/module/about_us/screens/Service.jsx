5377b4b76856397d9ebc88acee2459364ea98fd1
friendship0105 <tom9884661@gmail.com>
2023-07-09 15:32:39 +0800

    Service2

--- src/web/module/about_us/screens/Service.jsx
+++ src/web/module/about_us/screens/Service.jsx
@@ -16,8 +16,28 @@ export default function Service() {
             <button className="service-button">桃園</button>
             <button className="service-button">全部</button>
           </div>
+          <h1 className="service-heading">國內</h1>
+          <div className="button-container">
+            <button className="service-button">越南</button>
+            <button className="service-button">菲律賓</button>
+          </div>
+          <h1 className="service-heading">服務類型</h1>
+            <div className="button-row">
+              <button className="service-button">國際關懷</button>
+              <button className="service-button">環保標題</button>
+              <button className="service-button">偏鄉服務</button>
+            </div>
+            <div className="button-row">
+              <button className="service-button">社區服務</button>
+              <button className="service-button">科技服務</button>
+              <button className="service-button">地方創生</button>
+            </div>
+            <div className="button-row">
+              <button className="service-button">教育輔導</button>
+              <button className="service-button">創意設計</button>
+            </div>
         </div>
-        <img src={tw} alt="台灣拚完圖片" className="service-image" />
+      <img src={tw} alt="台灣拚完圖片" className="service-image" />
       </div>
     </div>
   );
