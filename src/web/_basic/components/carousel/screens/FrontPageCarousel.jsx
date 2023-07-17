90565c416d3b9fd57438d56a97a026c92ac9e6aa
suenyng <su.yu.grace@gmail.com>
2023-07-18 00:26:38 +0800

    images

--- src/web/_basic/components/carousel/screens/FrontPageCarousel.jsx
+++ src/web/_basic/components/carousel/screens/FrontPageCarousel.jsx
@@ -7,8 +7,8 @@ import { settings } from "../../settings/setting_frontpage";
 export default function FronPageCarousel(){
 
     const photos = [
-        { id: 1, src: require("../images/FrongPage_5.png") },
-        { id: 2, src: require("../images/FrongPage_2.png") },
+        { id: 1, src: require("../images/FrongPage_2.png") },
+        { id: 2, src: require("../images/FrongPage_5.png") },
         { id: 3, src: require("../images/FrongPage_4.png") },
         { id: 4, src: require("../images/GMM_1.png") },
 
