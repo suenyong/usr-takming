d3137eaecb5459a662d028b90c11e8fd9b6f16bd
suenyng <su.yu.grace@gmail.com>
2023-07-29 21:04:44 +0800

    ch

--- src/web/module/home/components/UsrUsrHub/UsrUsrHub.jsx
+++ src/web/module/home/components/UsrUsrHub/UsrUsrHub.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import '../UsrUsrHub/sryles/UsrUsrHub.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import the AOS CSS for default animations
+import { Route, Routes, Link } from "react-router-dom";
+import UH109 from '../../../USRHUB/screens/UH109';
 
 export default function UsrUsrHub(){
 
@@ -25,13 +27,24 @@ return (
         </h1>
 
     <div className="container-push">
-        <button className="textUSRHUB-button" data-aos="slide-right">
-        USR HUB
-        </button>
-        <button className="textUSR-button" data-aos="slide-right">
+        <Link to="/UH109" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <button className="textUSRHUB-button" 
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine">
+                USR HUB
+            </button>
+        </Link>
+        <button className="textUSR-button" 
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine">
         USR
         </button>
     </div>
+    <Routes>
+        <Route path="/UH109" element={<UH109 />} />
+    </Routes>
     </>
 );
 };
