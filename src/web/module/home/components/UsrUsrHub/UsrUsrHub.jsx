5621bab2b2bd9065624ec5ba4cc1f291902d6930
suenyng <su.yu.grace@gmail.com>
2023-07-31 18:07:59 +0800

    GMM finish

--- src/web/module/home/components/UsrUsrHub/UsrUsrHub.jsx
+++ src/web/module/home/components/UsrUsrHub/UsrUsrHub.jsx
@@ -4,6 +4,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import the AOS CSS for default animations
 import { Route, Routes, Link } from "react-router-dom";
 import UH109 from '../../../USRHUB/screens/UH109';
+import GMM from '../../../USR/screens/GMM';
 
 export default function UsrUsrHub(){
 
@@ -36,15 +37,18 @@ return (
                 USR HUB
             </button>
         </Link>
+        <Link to='/GMM' target="_blank" style={{ textDecoration: 'none', color: 'inherit', width:"10%" }}>
         <button className="textUSR-button" 
             data-aos="fade-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine">
         USR
         </button>
+        </Link>
     </div>
     <Routes>
         <Route path="/UH109" element={<UH109 />} />
+        <Route path="/GMM" element={<GMM />} />
     </Routes>
     </>
 );
