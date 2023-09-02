5fd155dc64d0672855e688f1accf4a8807b1149e
takming <110takmingusr@gmail.com>
2023-09-02 23:06:35 +0800

    www

--- src/web/module/home/components/AllName/AllName.jsx
+++ src/web/module/home/components/AllName/AllName.jsx
@@ -8,8 +8,8 @@ export default function AllName() {
     <>
         <div className="allName">
             <p>@USR官網由 德明財經科技大學 資訊管理系113學年度畢業專題生開發<br/>
-            ZHANG SHU TING, TANG ZONG RAY,NI MIN ZHU, 
-            JIANG MING FANG, LEE TING HSUAN, SU EN YONG</p>
+             ZHANG SHU TING, TANG ZONG RAY, NI MIN ZHU, 
+             JIANG MING FANG, LEE TING HSUAN, SU EN YONG</p>
         </div>
     </>
     );
