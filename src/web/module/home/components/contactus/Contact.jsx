f233761988e3277c1c5383f21670ddab09bdd377
suenyng <su.yu.grace@gmail.com>
2023-09-16 20:04:07 +0800

    frontpageimage

--- src/web/module/home/components/contactus/Contact.jsx
+++ src/web/module/home/components/contactus/Contact.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
         <div className="text-container">
           <h3 className="contact-text">德明財經科技大學研究發展處</h3>
           <h3 className="contact-text">學校地址：11451台北市內湖區環山路一段56號</h3>
-          <h3 className="contact-text">電子郵件：aged101@takming.edu.tw</h3>
+          <h3 className="contact-text">電子郵件：hwn8157@takming.edu.tw</h3>
           <h3 className="contact-text">電話：(02)26585801</h3>
           <h3 className="contact-text">研究處分機：2401</h3>
         </div>
